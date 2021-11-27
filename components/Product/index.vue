71b8110139f63e77e40b69f41d75a5bd7096d0a9
David Vigo <dvigo@pop-os.localdomain>
2021-11-27 23:49:07 +0100

    Add products

---
+++ components/Product/index.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  v-col(sm="6" md="4" lg="3" cols="12")
+    v-card.productCard(style="margin:10px")
+      v-img(:src="product.photo_url")
+      v-card-title {{product.name}}
+      v-card-text {{ product.description}}
+      v-card-action
+        v-icon mdi-plus
+</template>
+
+<script> 
+export default {
+  props: {
+      product: {
+          type: Object
+      }
+  }
+}
+</script>
---
+++ pages/products.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  v-app
+    v-row
+      Product(v-for="product in products" v-bind:product="product")
+</template>
+
+<script>
+import Product from '~/components/Product/index.vue'
+
+export default {
+  data () {
+    return {
+      products: Object
+    }
+  },
+  components: {
+    Product
+  },
+  async fetch() {
+    await this.$axios.$get('/product').then( (res) => this.products = res.product);
+  }
+}
+</script>

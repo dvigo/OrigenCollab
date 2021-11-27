f2626544ddddd4bce2a282532ec5a40610b9a26b
Xavier Codinas <xavier19966@gmail.com>
2021-11-27 21:53:37 +0100

    Update cart controller to include yjs

--- components/Cart/index.vue
+++ components/Cart/index.vue
@@ -6,33 +6,13 @@
           h2.text-center Cistella
       v-row.products
         v-col
-          v-banner
-          v-card.cart_product
-            v-card-title Product 1
-          v-card.cart_product
-            v-card-title Product 2
-          v-card.cart_product
-            v-card-title Product 2
-          v-card.cart_product
-            v-card-title Product 2
-          v-card.cart_product
-            v-card-title Product 2
-          v-card.cart_product
-            v-card-title Product 2
-          v-card.cart_product
-            v-card-title Product 2
-          v-card.cart_product
-            v-card-title Product 2
-          v-card.cart_product
-            v-card-title Product 2
-          v-card.cart_product
-            v-card-title Product 2
-          v-card.cart_product
-            v-card-title Product 2
-          v-card.cart_product
-            v-card-title Product 2
-          v-card.cart_product
-            v-card-title Product 2
+
+          div#cart-products(v-for="p in cart")
+            v-banner
+              v-card-title {{p[1]['product'].name }}
+            v-card.cart_product
+              v-card-title {{p[1]['quantity'] * p[1]['product'].price + '€'}}
+              v-card-body {{p[1]['quantity'] + 'x' + p[1]['product'].price + '€'}}
       v-row.cart_action
         v-col
           v-btn Tramitar pedido
@@ -41,8 +21,110 @@
 
 </template>
 
-<script> 
+<script>
+import * as Y from 'yjs'
+import { WebsocketProvider } from 'y-websocket'
+
 export default {
-  
+    components: {
+    },
+    data () {
+        return {
+            user: this.$auth.user,
+            cart: [],
+        }
+    },
+    mounted() {
+        this.loadYji();
+    },
+    computed: {
+        totalPrice() {
+            var price = 0;
+            Array.from(this.cart.values()).forEach(function(value, i) {
+                price += value.product.price * value.quantity
+            })
+            return price
+        },
+    },
+    methods: {
+        getRandomColor() {
+            var letters = '0123456789ABCDEF';
+            var color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        },
+        findGetParameter(parameterName) {
+            var result = null,
+                tmp = [];
+            location.search
+                .substr(1)
+                .split("&")
+                .forEach(function (item) {
+                tmp = item.split("=");
+                if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+                });
+            return result;
+        },
+        async getProduct(productId) {
+            var product_filtered = this.products.filter(p => p.id === productId)
+            if (product_filtered.length <= 0) {
+                // Fetch from api
+                return {id: 2, name: 'Product 2', price: 10, description: 'teskto'}
+            }
+            return product_filtered[0]
+        },
+        async loadYji() {
+            const ydoc = new Y.Doc()
+            this.cart = ydoc.getMap('cart')
+            this.cart.observe(event => {
+                this.$forceUpdate();
+
+            })
+
+            const provider = new WebsocketProvider('ws://localhost:1234',
+                this.findGetParameter('room') ||  'no-room', ydoc)
+
+            provider.awareness.setLocalStateField('user', {
+                    name: this.user ? this.user.first_name + ' ' + this.user.last_name:'User ' + this.getRandomColor(),
+                color: this.getRandomColor()
+            })
+            provider.awareness.on('change', () => {
+                // Map each awareness state to a dom-string
+                const strings = []
+                provider.awareness.getStates().forEach(state => {
+                    if (state.user) {
+                        strings.push(`<li style="color:${state.user.color};">${state.user.name}</li>`)
+                    }
+                    document.querySelector('#cart-users').innerHTML = strings.join('')
+                })
+            })
+
+        },
+        async updateCart(product, quantity) {
+            this.$forceUpdate();
+            if (!quantity) {
+                quantity = document.getElementById('add-product-quantity-' + product.id).value || 1
+            }
+            quantity = parseInt(quantity)
+            const existing_product = this.cart.get(product.id.toString())
+            if (existing_product) {
+                this.cart.set(product.id.toString(), {
+                            product: product,
+                            quantity: existing_product.quantity + quantity,
+                            })
+            } else {
+                this.cart.set(product.id.toString(), {
+                            product: product,
+                            quantity: quantity,
+                            })
+            }
+        },
+        async deleteProductCart(productId) {
+            this.cart.delete(productId.toString())
+        },
+    }
 }
+
 </script>

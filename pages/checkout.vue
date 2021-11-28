01213b2a422f6558cc35b85e3c4353830d8c7a09
David Vigo <dvigo@pop-os.localdomain>
2021-11-28 06:30:45 +0100

    Create checkout

---
+++ pages/checkout.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  v-app
+    v-row
+      v-spacer
+      v-col(cols="12" xs="9" md="8" lg="6")
+        Cart
+      v-spacer
+</template>
+
+<script>
+import Cart from '~/components/Cart/index.vue'
+
+export default {
+  data () {
+    return {
+      products: Object
+    }
+  },
+  components: {
+    Cart
+  },
+}
+</script>
